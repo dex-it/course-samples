f33aee2cd0ba16b931fd68884ac9b37446ca439d
viawork <41791593+viawork@users.noreply.github.com>
2019-10-25 08:25:22 +0300

    добавлен коммент

--- Samples/Stage1App/Program.cs
+++ Samples/Stage1App/Program.cs
@@ -12,6 +12,7 @@ namespace StageConsole1App
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, All");
+            // комент
             Console.ReadLine();
         }
     }
