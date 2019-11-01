14f8617637666cc336f61e0116e22f1609bf9e40
Sergey Revo <sergey.revo@outlook.com>
2019-11-01 14:12:43 +0200

    AstroEnumerator

---
+++ Samples/Stage1Chapter7/AstronomicalObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage1Chapter7
+{
+    class AstronomicalObject
+    {
+        public string Name { get; }
+        public bool LightEmission { get; }
+        public AstronomicalObject(string name = "No name", bool lightEmission = false)
+        {
+            Name = name;
+            LightEmission = lightEmission;
+        }
+
+    }
+}
---
+++ Samples/Stage1Chapter7/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage1Chapter7
+{
+    class StellarSystem
+    {
+
+        class AstroEnumerator : IEnumerator
+        {
+            readonly AstronomicalObject[] astro;
+            int position = -1;
+            public AstroEnumerator(AstronomicalObject[] astro)
+            {
+                this.astro = astro;
+            }
+            public object Current
+            {
+                get
+                {
+                    return astro[position];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (position < astro.Length - 1)
+                {
+                    position++;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+        class SolarSystem
+        {
+
+            readonly AstronomicalObject[] astro = {
+                    new AstronomicalObject("Солнце", true),
+                    new AstronomicalObject("Меркурий", false),
+                    new AstronomicalObject("Венера", false),
+                    new AstronomicalObject("Земля", false),
+                    new AstronomicalObject("Марс", false),
+                    new AstronomicalObject("Юпитер", false),
+                    new AstronomicalObject("Сатурн", false),
+                    new AstronomicalObject("Уран", false),
+                    new AstronomicalObject("Нептун", false) };
+
+            public IEnumerator GetEnumerator()
+            {
+                return new AstroEnumerator(astro);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SolarSystem solarSystem = new SolarSystem();
+            
+            foreach (AstronomicalObject a in solarSystem)
+            {
+
+                if (a.LightEmission) {
+                    Console.WriteLine(a.Name + " - это звезда." );
+                }
+                else
+                {
+                    Console.WriteLine(a.Name + " - это планета.");
+                }
+                    
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
