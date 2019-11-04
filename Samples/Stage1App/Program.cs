802cb0d44cca8de734d740241fb225c89fd474d1
viawork <41791593+viawork@users.noreply.github.com>
2019-11-04 21:56:05 +0200

    попробовал многопоточность

--- Samples/Stage1App/Program.cs
+++ Samples/Stage1App/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 
@@ -9,11 +10,33 @@ namespace StageConsole1App
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hi, All");
-            // комент
-            Console.ReadLine();
+
+            // 
+            string str = "Пример побуквенного вывода строки";
+            foreach (char o in str)
+            {
+                Console.Write(o);
+            }
+            var t = new Thread(PrintY);
+            t.Start();
+
+            while (true)
+                Console.Write("x?");
+
+            _ = Console.ReadLine();
+        }
+      //  [Timeout(100)]
+        static void PrintY()
+        {
+            while (true)
+            {
+                Console.Write("y!");
+            
+                }
         }
     }
 }
