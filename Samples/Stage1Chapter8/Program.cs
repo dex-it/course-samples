6aaa60d7d34994b21d305253440d32caaf0a1b39
Revo69 <sergey.revo@outlook.com>
2019-11-04 21:49:55 +0200

    LINQ

---
+++ Samples/Stage1Chapter8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage1Chapter8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] teams = { "Бавария", "Боруссия", "Реал Мадрид", "Манчестер Сити", "ПСЖ", "Барселона" };
+
+            var selectedTeams = new List<string>();
+            foreach (string s in teams)
+            {
+                if (s.ToUpper().StartsWith("Б"))
+                    selectedTeams.Add(s);
+            }
+            selectedTeams.Sort();
+
+            foreach (string s in selectedTeams)
+                Console.WriteLine(s);
+
+        }
+    }
+}
