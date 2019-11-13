3b12275b9a5b9e64c1673e8e54cc4413a873dcf7
Alexander.Bolokan <alexander.bolokan@mdaoperator.com.au>
2019-11-13 23:03:53 +0200

    Extestions file

---
+++ Samples/Dex.ABolokan.Services/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dex.ABolokan.Services
+{
+	public static class TimeExtensions
+	{
+		public static double Seconds(this TimeSpan timeSpan)
+		{
+			return timeSpan.TotalSeconds;
+		}
+
+		public static TimeSpan GetTimeSpan(this double seconds)
+		{
+			return TimeSpan.FromSeconds(seconds);
+		}
+	}
+}
