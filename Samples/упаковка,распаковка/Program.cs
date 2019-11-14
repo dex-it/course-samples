8fec7986ca0dd001f9830aba051cbf6c159b568a
Влад Зброжек <pointbreak22@outlook.com>
2019-11-14 21:41:24 +0200

    Упаковка и расспаковка

---
+++ Samples/упаковка,распаковка/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace упаковка_распаковка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1000;
+           Stopwatch st = new Stopwatch();
+            st.Start();
+            object o = i;
+
+            st.Stop();
+            string time1 = st.ElapsedTicks.ToString();
+            st.Restart();
+          
+            i = (int)o;
+            st.Stop();
+            string time2 = st.ElapsedTicks.ToString();
+            Console.WriteLine("Время упаковки в тиках:" + time1 + " Время расспаковки:" + time2);
+            Console.ReadLine();
+
+        }
+
+    }
+}
