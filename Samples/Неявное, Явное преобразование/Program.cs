ded27b4e571ba39c8898593d8047978faf36eaca
Влад Зброжек <pointbreak22@outlook.com>
2019-11-14 21:19:49 +0200

    преобразование

---
+++ Samples/Неявное, Явное преобразование/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Неявное__Явное_преобразование
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя и фамилию в строку через пробел");
+            string str = Console.ReadLine();
+            Person person = new Person { FirstName = str.Split(' ')[0], LastName = str.Split(' ')[1]};
+
+          //  var sr1 = (str == Person)?true:false;
+        }
+    }
+    class Person
+    {
+       public string FirstName { get; set; }
+      public string LastName { get; set; }
+
+      
+    }
+}
