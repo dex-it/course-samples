8dddd6c446b993a827e39628cf06e5d83ad6d298
mikhailov.se <mikhailov.se@hotmail.com>
2019-11-15 06:40:00 +0200

    MyClonable

--- Samples/Stage1/MYClonableTest.cs
+++ Samples/Stage1/MYClonableTest.cs
@@ -5,25 +5,46 @@ namespace Stage1
 {
     public class MyClonableTest
     {
-       public class Car : ICloneable
+       private class Car : ICloneable
        {
            public int Id  { get; set; }
            public string Name  { get; set; }
            public string Number  { get; set; }
 
+           public Owner Owner { get; set; }
+           
+
            public object Clone()
            {
-//               return new Car
-//               {
-//                   Name = this.Name,
-//                   Id = this.Id,
-//                   Number = this.Number
-//               };
-
                return this.MemberwiseClone();
+           }
+
+           public Car DeepClone()
+           {
+               return new Car
+               {
+                   Name = this.Name,
+                   Id = this.Id,
+                   Number = this.Number,
+                   Owner = new Owner("ClonedOwner")
+               };
            }
        }
        
+       private class Owner
+       {
+           public string Name { get; set; }
+
+           public Owner(string name)
+           {
+               Name = name;
+           }
+
+           public Owner()
+           {
+               
+           }
+       }
 
        [Test]
        public void ClonableTest()
@@ -32,17 +53,28 @@ namespace Stage1
            {
                 Id = 1,
                 Name = "1",
-                Number = "1"
+                Number = "1",
+                Owner = new Owner("Owner1")
            };
 
            var car2 = car1;
-           car2.Id = 1;
-           Assert.AreEqual(car1.Id, car2.Id);
 
-           car2 = car1.Clone() as Car;
-           car2.Id = 2;
-           Assert.AreNotEqual(car1.Id, car2.Id);
+           Assert.AreEqual(car1.Owner, car2.Owner);
+               
            
+           car2 = car1.Clone() as Car;
+
+           Assert.AreEqual(car1.Owner, car2.Owner);
+
+           
+           car2 = car1.DeepClone();
+
+           Assert.AreNotEqual(car1.Owner, car2.Owner);
+           
+
        }
+       
+       
+       
     }
 }
