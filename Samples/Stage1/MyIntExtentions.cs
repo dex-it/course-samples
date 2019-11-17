b21f4d8b738d8b69efa3cffb71b6abaf0bf1d313
mikhailov.se <mikhailov.se@hotmail.com>
2019-11-17 17:27:47 +0200

    MyExstention

---
+++ Samples/Stage1/MyIntExtentions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Stage1
+{
+    //        Реализовать методы расширения для класса int, 
+    //        для операций над TimeSpan. Например 1,Seconds() = (TimeSpan) 00.00.01;
+
+    public static class MyIntExtentions
+    {
+        public static TimeSpan Seconds(this int i)
+        {
+            return TimeSpan.FromSeconds(i);
+        }
+    }
+
+    public class MyIntExstentionTest
+    {
+        [Test]
+        public void IntExstentionTest()
+        {
+            Assert.AreEqual(1.Seconds(),new TimeSpan(00,00,01));
+        }
+    }
+}
