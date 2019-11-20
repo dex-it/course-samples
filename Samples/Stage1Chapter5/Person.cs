70d610016356381631943404570d6b79db98320d
Sergey Revo <sergey.revo@outlook.com>
2019-11-20 13:58:59 +0200

    Cast

---
+++ Samples/Stage1Chapter5/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace Stage1Chapter5
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName = "", string lastName = "")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        // Неявное преобразование типа Person к string
+        public static implicit operator string(Person obj)
+        {
+            return obj.FirstName + " " + obj.LastName;
+        }
+
+        // Явное преобразование типа string к Person
+        public static explicit operator Person(string obj)
+        {
+            string[] words = obj.Split(' ');
+
+            if (words.Length == 2 )
+            {
+                return new Person(words[0], words[1]);
+            }
+            else if(words.Length == 1)
+            {
+                return new Person(words[0], "");
+            }
+            else
+            {
+                return new Person();
+            }
+            
+            
+        }
+    }
+}
---
+++ Samples/Stage1Chapter5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stage1Chapter5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person discovererOfPluto = new Person("Michael", "Brown");
+
+            string str = "Michael Brown";
+
+            Console.WriteLine("Проверка на эквивалентность c помощью оператора == : " + (str == discovererOfPluto).ToString());
+
+            Console.WriteLine("Проверка на эквивалентность c помощью метода Equals() : " + discovererOfPluto.Equals(str).ToString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
