26f5f4212a2d7adfbeac5d203556b57197172350
Dem-Ivan <demidenko.vanya@list.ru>
2019-11-20 19:12:33 +0300

    add Topic_5(boxing-and-unboxing-Time)

---
+++ Topic_5(boxing-and-unboxing-Time)/Topic_5(boxing-and-unboxing-Time)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Topic_5_boxing_and_unboxing_Time_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			int a = 123;
+			int b;
+			object Sempl1;
+			long t1;
+			long t2;
+			long dt;
+
+			Console.WriteLine("_ _ _ boxing _ _ _");
+			t1 = DateTime.Now.Ticks;
+			Sempl1 = a;
+			t2 = DateTime.Now.Ticks;
+			dt = t2 - t1;
+			Console.WriteLine($"dt = {dt} ticks    t1 = {t1} t2 = {t2}");
+
+			Console.WriteLine("_ _ _ unboxing _ _ _");			
+			t1 = DateTime.Now.Ticks;
+			b = (int)Sempl1;
+			t2 = DateTime.Now.Ticks;
+			dt = t2 - t1;
+			Console.WriteLine($"dt = {dt} ticks    t1 = {t1} t2 = {t2}");
+
+		}
+	}
+}
