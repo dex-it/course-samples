65df074660dcfcdb73eecc97a9c9e87ac2223848
Sergey Revo <sergey.revo@outlook.com>
2019-11-20 15:43:27 +0200

    Boxing

---
+++ Samples/Stage1Chapter6/Packing.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Stage1Chapter6
+{
+    public class Packing
+    {
+        private readonly Random _rnd = new Random();
+        private readonly int MaxValue = 10000000;
+
+        public int MakeValueInt()
+        {
+            return _rnd.Next(0, MaxValue);
+        }
+
+        public object MakeValueObject()
+        {
+            return _rnd.Next(0, MaxValue);
+        }
+
+        public int[] RetrieveInt()
+        {
+            int[] values = new int[MaxValue];
+            for (var i = 0; i < MaxValue; i++)
+            {
+                values[i] = MakeValueInt();
+            }
+            return values;
+        }
+
+        public object[] RetrieveObject()
+        {
+            object[] values = new object[MaxValue];
+            for (var i = 0; i < MaxValue; i++)
+            {
+                values[i] = MakeValueObject();
+            }
+            return values;
+        }
+
+        public void Boxing(int[] values)
+        {
+            object box = new object();
+            for (var i = 0; i < MaxValue; i++)
+            {
+                box = values[i];
+            }
+        }
+        public void UnBoxing(object[] values )
+        {
+            int unbox;
+            for (var i = 0; i < MaxValue; i++)
+            {
+                unbox = (int)values[i];
+            }
+        }
+        public void Copy(int[] values)
+        {
+            int copy;
+            for (var i = 0; i < MaxValue; i++)
+            {
+                copy = values[i];
+            }
+        }
+
+    }
+}
--- Samples/Stage1Chapter6/Program.cs
+++ Samples/Stage1Chapter6/Program.cs
@@ -1,10 +1,36 @@
-﻿
+﻿using System;
+using System.Diagnostics;
+
 namespace Stage1Chapter6
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Packing example = new Packing();
+            int[] arrInt = example.RetrieveInt();
+            object[] arrObj = example.RetrieveObject();
+
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            example.Boxing(arrInt);
+            stopWatch.Stop();
+            Console.WriteLine("Упаковка: " + stopWatch.ElapsedMilliseconds);
+
+            stopWatch.Reset();
+            stopWatch.Start();
+            example.UnBoxing(arrObj);
+            stopWatch.Stop();
+            Console.WriteLine("Распаковка: " + stopWatch.ElapsedMilliseconds);
+
+            stopWatch.Reset();
+            stopWatch.Start();
+            example.Copy(arrInt);
+            stopWatch.Stop();
+            Console.WriteLine("Копирование: " + stopWatch.ElapsedMilliseconds);
+
+            Console.ReadLine();
 
         }
     }
