019e0e0ba4450909e0e2abf4666c76cd6fd29100
VyasheslavKovalch <vyacheslavkovalch@gmail.com>
2019-11-22 00:08:49 +0200

    Add Stage1Paragraph11

--- Samples/Stage1Paragraph11/Program.cs
+++ Samples/Stage1Paragraph11/Program.cs
@@ -1,21 +1,55 @@
-﻿using System;
+﻿using Stage1Paragraph10;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Stage1Paragraph11
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static void FillJobSprav(Dictionary<Person, string> sprav)
         {
-            //Dictionary<string, int> dictionary = new Dictionary<string, int>();
-            //dictionary["AB"] = 20;
+            // Заполняем словарь
+            sprav[new Person("Николай", new DateTime(1970, 01, 01), "Москва", 1111111)] = "Рога и копыта";
+            sprav[new Person("Анатолий", new DateTime(1975, 01, 01), "Таганрог", 3333333)] = "Ресторан Плакучая ива";
+            sprav[new Person("Джорж", new DateTime(1985, 01, 01), "Техас", 2222222)] = "Голивуд";
+            sprav[new Person("Сергей", new DateTime(1990, 01, 01), "Тирасполь", 9999999)] = "Шериф";
+            sprav[new Person("Александр", new DateTime(1992, 01, 01), "Москва", 5555555)] = "Microsoft";
+            sprav[new Person("Григорий", new DateTime(1978, 01, 01), "Новосибирск", 4444444)] = "Газпром";
+            sprav[new Person("Санду", new DateTime(1960, 01, 01), "Кишинев", 7777777)] = "Orange";
+            sprav[new Person("Тарас", new DateTime(1988, 01, 01), "Киев", 88888888)] = "конфетная фабрика Рошен";
+        }
 
-            //Console.WriteLine(dictionary["AB "]);
+        private static void FindPlaceOfJob(Dictionary<Person, string> sprav, Person person)
+        {
+            foreach (var personPair in sprav)
+            {
+                if (personPair.Key == person)
+                {
+                    Console.WriteLine($"Искомое место работы: {personPair.Value}");
+                    return;
+                }
+            }
 
-            //dictionary.
+            Console.WriteLine("Место работы не найдено!");
+        }
+
+        static void Main()
+        {
+            // заполняем справочник
+            var jobSprav = new  Dictionary<Person, string>();
+            FillJobSprav(jobSprav);
+
+            // ищем место работы
+            var sourcePerson = new Person("Григорий", new DateTime(1978, 01, 01), "Новосибирск", 4444444);
+            FindPlaceOfJob(jobSprav, sourcePerson);
+
+            sourcePerson = new Person("Григорий", new DateTime(1978, 01, 01), "Новосибирск", 0000000);
+            FindPlaceOfJob(jobSprav, sourcePerson);
+
+            // Вопрос: Как правильно обработать в этом случае ошибочную ситуацию, если будет sourcePerson = null ?
+
+            Console.ReadLine();
         }
     }
 }
