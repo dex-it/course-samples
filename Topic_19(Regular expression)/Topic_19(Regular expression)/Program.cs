5e85d368137882b2179ae3d911902857e52816c8
Dem-Ivan <demidenko.vanya@list.ru>
2019-11-21 17:26:27 +0300

    changed Topic_19(Regular expression)

--- Topic_19(Regular expression)/Topic_19(Regular expression)/Program.cs
+++ Topic_19(Regular expression)/Topic_19(Regular expression)/Program.cs
@@ -1,79 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Topic_19_Regular_expression_
 {
 	class Program
 	{
-		static MatchCollection f(string text, string start = "", string end="")
-		{
-			Regex regexSurname = new Regex($@"${start}\s\w*{end}");
-			return regexSurname.Matches(text);
-		}
 		static void Main(string[] args)
-		{
-			
-			string clientInformation = "Петров Сергей Генадиевич. тел.: 077951206. Женат ";
-			string name = "";
-			Console.WriteLine($"Исследуемые данные о клиенте: {clientInformation}");
+		{			
+			string clientInformation = "Петров Сергей Генадиевич, тел.: +37377951206, Женат, счет: 100.23.1000, сумма: 1 000 000";
+						
+			Console.WriteLine($"Исследуем данные клиента: {clientInformation}");
 			Console.WriteLine("_ _");
+			Dictionary<string,string> Customer = CustomerResearch(clientInformation);			
 
+			foreach (var item in Customer)
+			{
+				Console.WriteLine($"{item.Key} = {item.Value}");
+			}
+			Console.WriteLine("_ _");
+			Console.WriteLine("Производим замену семейного положения клиента");
+			ChangeMaritalStatus(clientInformation);	
 
-
+		}
+		public static Dictionary<string, string> CustomerResearch(string clientInformation)
+		{
+			//Исследуем данные клиента
+			Dictionary<string, string> Client = new Dictionary<string, string>();
 			Regex regexSurname = new Regex(@"\w*ов");
 			Regex regexName = new Regex(@"ов\s\w*");
 			Regex regexName2 = new Regex(@"\s\w*");
 			Regex regexPatronymic = new Regex(@"(\w*вич)|(\w*вна)");
 			Regex regexFonNumber = new Regex(@"[7]{2}[0-9]{6}");
-			Regex regexMaritalStatus = new Regex(@"Женат|Холост",RegexOptions.IgnoreCase);
-
-			MatchCollection matchesSurname = regexSurname.Matches(clientInformation);
-			MatchCollection matchesName = regexName.Matches(clientInformation);
-			MatchCollection matchesName2;
-			MatchCollection matchesPatronymic = regexPatronymic.Matches(clientInformation);
-			MatchCollection matchesFonNumber = regexFonNumber.Matches(clientInformation);
-			MatchCollection matchesMaritalStatus = regexMaritalStatus.Matches(clientInformation);
-
-			foreach (Match Surname in matchesSurname)
-			{
-				Console.WriteLine($"Surname = {Surname}");
-			}
-
-			foreach (Match Name in  matchesName)
-			{
-				Console.WriteLine($"Name = { Name}");
-				name = Name.ToString();
-			}
-			matchesName2 = regexName2.Matches(name);
-			foreach (Match Name2 in matchesName2)
-			{
-				Console.WriteLine($"Name = { Name2}");
-			}
-
-
-			foreach (Match Patronymic in matchesPatronymic)
-			{
-				Console.WriteLine($"Patronymic = { Patronymic}");
-			}
-
-			foreach (Match FonNumber in matchesFonNumber)
-			{
-				Console.WriteLine($"FonNumber = {FonNumber}");
-			}
-
-			foreach (Match MaritalStatus in matchesMaritalStatus)
-			{
-				Console.WriteLine($"MaritalStatus = {MaritalStatus}");
-			}
-			Console.WriteLine("_ _");
-
-			Console.WriteLine("Производим замену семейного положения клиента");
+			Regex regexMaritalStatus = new Regex(@"Женат|Холост", RegexOptions.IgnoreCase);
+			Regex regexAccount = new Regex(@"\d{3}\.\d{2}\.\d{3}");
+			Regex regexSum = new Regex(@"([0-9]\d*\s[0-9]{3}\s[0-9]{3})");
+			string name = regexName.Match(clientInformation).ToString();
+			Client.Add("Surname", regexSurname.Match(clientInformation).ToString());
+			Client.Add("Name", regexName2.Match(name).ToString());
+			Client.Add("Patronymic", regexPatronymic.Match(clientInformation).ToString());
+			Client.Add("FonNumber", regexFonNumber.Match(clientInformation).ToString());
+			Client.Add("MaritalStatus", regexMaritalStatus.Match(clientInformation).ToString());
+			Client.Add("Account", regexAccount.Match(clientInformation).ToString());
+			Client.Add("Sum", regexSum.Match(clientInformation).ToString());
+			return Client;
+		}
+		public static void ChangeMaritalStatus(string clientInformation)
+		{
+			//Производим замену семейного положения клиента
 			string pattern = @"женат";
 			string targetMaritalStatus = "Холост";
-			Regex regex1 = new Regex(pattern, RegexOptions.IgnoreCase);
-			string result = regex1.Replace(clientInformation,targetMaritalStatus);
-			Console.WriteLine(result);
-			//Console.Read();
+			Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);			
+			Console.WriteLine(regex.Replace(clientInformation, targetMaritalStatus));
 		}
 	}
 }
