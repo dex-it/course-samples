0cda2d319bbca3d574e754ffae0f9ebb86f81f33
Dem-Ivan <demidenko.vanya@list.ru>
2019-11-21 22:19:32 +0300

    add Topic_19(Regular expression-WhiteSpaceCharacter )

---
+++ Topic_19(Regular expression-WhiteSpaceCharacter )/Topic_19(Regular expression-WhiteSpaceCharacter )/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace Topic_19_Regular_expression_WhiteSpaceCharacter__
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string text = "qweqwr  qerqet    qwrtttqw qwrqrt    wrwrw";
+			Console.WriteLine("Исходная строка:" + text);
+			Console.WriteLine("Результат:"+ DeleteWhiteSpace(text));
+			
+		}
+		public static string DeleteWhiteSpace(string text)
+		{
+			Regex regex = new Regex(@"[ ]+");
+			return (regex.Replace(text, " "));
+		}
+		
+	}
+}
