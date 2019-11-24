8becd5b748bfdee23a216f31a5c7b42e6a139d06
Revo69 <sergey.revo@outlook.com>
2019-11-24 23:29:09 +0200

    Dictionary

---
+++ Samples/Stage1Chapter11/PlanetBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Stage1Chapter3;
+using Stage1Chapter10;
+
+namespace Stage1Chapter11
+{
+    public class PlanetBook
+    {
+
+        public readonly Dictionary<AstronomicalObjectEqual, int> _planetDictionary = new Dictionary<AstronomicalObjectEqual, int>();
+
+        public void AddAstronomicalObject(AstronomicalObjectEqual planet, int numberFromSun)
+        {
+            _planetDictionary.Add(planet, numberFromSun);
+        }
+
+        public int GetNumberFromSun(AstronomicalObjectEqual planet)
+        {
+            int res = 0;
+
+            foreach (var p in _planetDictionary)
+            {
+
+                if (p.Key.GetHashCode() == planet.GetHashCode())
+                {
+                    res = p.Value;
+                }
+                
+            }
+
+            return res;
+        }
+
+
+    }
+}
---
+++ Samples/Stage1Chapter11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stage1Chapter3;
+using Stage1Chapter10;
+
+namespace Stage1Chapter11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlanetBook book = new PlanetBook();
+
+            var mercury = new AstronomicalObjectEqual("Mercury", 2439, false);
+            var venus = new AstronomicalObjectEqual("Venus", 6051, false);
+            var earth = new AstronomicalObjectEqual("Earth", 6371, false);
+            var mars = new AstronomicalObjectEqual("Mars", 3389, false);
+            var jupiter = new AstronomicalObjectEqual("Jupiter", 69911, false);
+            var saturn = new AstronomicalObjectEqual("Saturn", 58232, false);
+            var uranus = new AstronomicalObjectEqual("Uranus", 25362, false);
+            var neptune = new AstronomicalObjectEqual("Neptune", 24622, false);
+
+            book.AddAstronomicalObject(mercury, 1);
+            book.AddAstronomicalObject(venus, 2);
+            book.AddAstronomicalObject(earth, 3);
+            book.AddAstronomicalObject(mars, 4);
+            book.AddAstronomicalObject(jupiter, 5);
+            book.AddAstronomicalObject(saturn, 6);
+            book.AddAstronomicalObject(uranus, 7);
+            book.AddAstronomicalObject(neptune, 8);
+
+            Console.WriteLine("Number from Sun (Earth): " + book.GetNumberFromSun(earth));
+            Console.ReadLine();
+
+
+        }
+    }
+}
