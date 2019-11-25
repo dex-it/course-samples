a7ba7778a76c9a764d0dec891393e79a107e05bc
mmx003 <mmx003@gmail.com>
2019-11-25 18:27:56 +0200

    small fixes

---
+++ Topic_1(OOP)/Topic_1(OOP)/ICreature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topic_1_OOP_
+{
+    public interface ICreature
+    {
+        string Family { get; } // к кому  семейству отнсеться
+        int NumberOfLegs { get; } // количество конечностей
+        bool ItFlies { get; } //летает ли оно?
+
+        bool ItIsHungry(int timeOfLastFeeding, int feedingFrequency);
+
+        void ChangeCountOfLegs(int countOfLegs);
+    }
+}
