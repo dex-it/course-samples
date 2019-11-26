2bdd8d8b1a164a25360ced262ce8d6bf26b33985
Dem-Ivan <demidenko.vanya@list.ru>
2019-11-26 11:59:36 +0300

    changed Topic_16(Extension methods - TimeSpan)

--- Topic_16(Extension methods - TimeSpan)/Topic_16(Extension methods - TimeSpan)/MyIntExtension.cs
+++ Topic_16(Extension methods - TimeSpan)/Topic_16(Extension methods - TimeSpan)/MyIntExtension.cs
@@ -6,10 +6,10 @@ namespace Topic_16_Extension_methods___TimeSpan_
 {
 	public static class MyIntExtension
 	{
-		public static TimeSpan Seconds(this int intervali)
+		public static TimeSpan Seconds(this int interval)
 		{
-			TimeSpan timeSpan = new TimeSpan(0, 0, intervali);
-			//timeSpan = (TimeSpan.FromSeconds)interval;
+			TimeSpan timeSpan = new TimeSpan(0, 0, interval);
+			
 			return (timeSpan);
 		}
 		public static TimeSpan Minutes(this int interval)
