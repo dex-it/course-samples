a882878417c821607082708b1525c9de86c787fc
Dem-Ivan <demidenko.vanya@list.ru>
2019-11-26 14:11:19 +0300

    changed Topic_5(boxing-and-unboxing-Time)

--- Topic_5(boxing-and-unboxing-Time)/Topic_5(boxing-and-unboxing-Time)/Program.cs
+++ Topic_5(boxing-and-unboxing-Time)/Topic_5(boxing-and-unboxing-Time)/Program.cs
@@ -12,22 +12,21 @@ namespace Topic_5_boxing_and_unboxing_Time_
 			object Sempl1;
 			long t1;
 			long t2;			
-			TimeSpan processDuration;
-
+			TimeSpan procesDuration;
 
 			Console.WriteLine("_ _ _ boxing _ _ _");
 			t1 = DateTime.Now.Ticks;
 			Sempl1 = a;
 			t2 = DateTime.Now.Ticks;			
-			processDuration = new TimeSpan(t2 - t1);
-			Console.WriteLine($"processDuration = {processDuration.Ticks} ticks    t1 = {t1} t2 = {t2}");
+			procesDuration = new TimeSpan(t2 - t1);
+			Console.WriteLine($"processDuration = {procesDuration.Ticks} ticks    t1 = {t1} t2 = {t2}");
 
 			Console.WriteLine("_ _ _ unboxing _ _ _");			
 			t1 = DateTime.Now.Ticks;
 			b = (int)Sempl1;
 			t2 = DateTime.Now.Ticks;
-			processDuration = new TimeSpan(t2 - t1);
-			Console.WriteLine($"processDuration = {processDuration.Ticks} ticks    t1 = {t1} t2 = {t2}");
+			procesDuration = new TimeSpan(t2 - t1);
+			Console.WriteLine($"processDuration = {procesDuration.Ticks} ticks    t1 = {t1} t2 = {t2}");
 
 		}
 	}
