1b5316c3b395d000c476f32153d125812db1a4dd
Dem-Ivan <demidenko.vanya@list.ru>
2019-11-26 17:24:40 +0300

    changed Topic_20(Threading-Mass10M100M)

--- Topic_20(Threading-Mass10M100M)/Topic_20(Threading-Mass10M100M)/Program.cs
+++ Topic_20(Threading-Mass10M100M)/Topic_20(Threading-Mass10M100M)/Program.cs
@@ -13,12 +13,14 @@ namespace Topic_20_Threading_Mass10M100M_
 			int[] mass10M = new int[10000000];
 			int[] mass100M = new int[100000000];
 			int[] testmss = new int[100];
-			GenerationMass generationmass = new GenerationMass();	
-			
-			generationmass.SerialCalc(testmss);
-			generationmass.ParallelCalc(testmss);
+			GenerationMass generationmass = new GenerationMass();
 			generationmass.CalcEvent += CalcHandler;
+
+			generationmass.SerialCalc(testmss);
+			generationmass.ParallelCalc(testmss);			
 			generationmass.ParalelCalc2(testmss);
+
+			generationmass.CalcEvent -= CalcHandler;
 		}
 
 		public static void CalcHandler(int i, int[] mass)
