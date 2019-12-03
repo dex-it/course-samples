cea1c1a049175e23670c6c2b6935de5a2426d5a8
Sergey Revo <sergey.revo@outlook.com>
2019-12-03 16:42:27 +0200

    INotifyPropertyChanged

---
+++ Samples/Stage1Chapter14/AstronomicalObject.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Stage1Chapter14
+{
+    public class AstronomicalObject : INotifyPropertyChanged
+    {
+        private string _name = "";
+        private decimal _radius = 0;
+        private bool _lightEmission = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        public decimal Radius
+        {
+            get
+            {
+                return _radius;
+            }
+
+            set
+            {
+                if (_radius != value)
+                {
+                    _radius = value;
+                    NotifyPropertyChanged();
+                }
+                
+            }
+        }
+        public bool LightEmission
+        {
+            get
+            {
+                return _lightEmission;
+            }
+
+            set
+            {
+                if (_lightEmission != value)
+                {
+                    _lightEmission = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+        public AstronomicalObject(string name = "No name", decimal radius = 0, bool lightEmission = false)
+        {
+
+            _name = name;
+            _radius = radius;
+            _lightEmission = lightEmission;
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 4 * Math.PI * (double)_radius * (double)_radius;
+        }
+
+    }
+
+
+}
---
+++ Samples/Stage1Chapter14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Stage1Chapter14
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            AstronomicalObject astro = new AstronomicalObject();
+
+            
+            PropertyChangedEventHandler onPropertyChanged = delegate {
+                Console.WriteLine("Changed properties: ");
+                Console.WriteLine("Name: " + astro.Name + ", Radius: " + astro.Radius + ", LightEmission: " + astro.LightEmission);
+                Console.ReadLine();
+            };
+            
+            astro.PropertyChanged += onPropertyChanged;
+
+            astro.Name = "Earth";
+            astro.Radius = 6371;
+            astro.LightEmission = false;
+
+            astro.Name = "Mars";
+
+            astro.PropertyChanged -= onPropertyChanged;
+        }
+
+    }
+}
