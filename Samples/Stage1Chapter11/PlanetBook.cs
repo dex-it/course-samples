19355959fa97c3e59bd4e957ac9fdda6a4ec33bf
Revo69 <sergey.revo@outlook.com>
2019-12-03 23:05:18 +0200

    Events

--- Samples/Stage1Chapter11/PlanetBook.cs
+++ Samples/Stage1Chapter11/PlanetBook.cs
@@ -6,14 +6,34 @@ namespace Stage1Chapter11
 {
     public class PlanetBook
     {
+        public int MaxRange { get; set; } = 0;
 
         public readonly Dictionary<AstronomicalObjectEqual, int> _planetDictionary = new Dictionary<AstronomicalObjectEqual, int>();
+
+
+        public event EventHandler ListEmptyReached;
+        public event EventHandler ListMaxRangeReached;
+
+        protected virtual void OnListEmptyReached(EventArgs e)
+        {
+            EventHandler handler = ListEmptyReached;
+            handler?.Invoke(this, e);
+        }
+        protected virtual void OnListMaxRangeReached(EventArgs e)
+        {
+            EventHandler handler = ListMaxRangeReached;
+            handler?.Invoke(this, e);
+        }
 
         public void AddAstronomicalObject(AstronomicalObjectEqual planet, int numberFromSun)
         {
             if (!_planetDictionary.ContainsKey(planet))
             {
                 _planetDictionary.Add(planet, numberFromSun);
+                if (_planetDictionary.Count > MaxRange)
+                {
+                    ListMaxRangeReached?.Invoke(this, EventArgs.Empty);
+                }
             }
             else
             {
@@ -26,6 +46,10 @@ namespace Stage1Chapter11
             if (_planetDictionary.ContainsKey(planet))
             {
                 _planetDictionary.Remove(planet);
+                if (_planetDictionary.Count == 0)
+                {
+                    ListEmptyReached?.Invoke(this, EventArgs.Empty);
+                }
             }
             else
             {
@@ -48,6 +72,7 @@ namespace Stage1Chapter11
 
             return res;
         }
+
 
 
     }
--- Samples/Stage1Chapter14/Program.cs
+++ Samples/Stage1Chapter14/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.ComponentModel;
+using Stage1Chapter10;
+using Stage1Chapter11;
 
 namespace Stage1Chapter14
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            /*
+            // 1
             AstronomicalObject astro = new AstronomicalObject();
-
             
             PropertyChangedEventHandler onPropertyChanged = delegate {
                 Console.WriteLine("Changed properties: ");
@@ -26,6 +28,44 @@ namespace Stage1Chapter14
             astro.Name = "Mars";
 
             astro.PropertyChanged -= onPropertyChanged;
+            */
+
+            // 2
+            PlanetBook book = new PlanetBook();
+
+            var mercury = new AstronomicalObjectEqual("Mercury", 2439, false);
+            var venus = new AstronomicalObjectEqual("Venus", 6051, false);
+            var earth = new AstronomicalObjectEqual("Earth", 6371, false);
+            var mars = new AstronomicalObjectEqual("Mars", 3389, false);
+
+            book.MaxRange = 3;
+
+            EventHandler OnListMaxRangeReached = delegate {
+                Console.WriteLine("Max Range");
+                Console.ReadLine();
+            };
+
+            EventHandler OnListEmptyReached = delegate {
+                Console.WriteLine("List Empty");
+                Console.ReadLine();
+            };
+
+            book.ListMaxRangeReached += OnListMaxRangeReached;
+            book.ListEmptyReached += OnListEmptyReached;
+
+            book.AddAstronomicalObject(mercury, 1);
+            book.AddAstronomicalObject(venus, 2);
+            book.AddAstronomicalObject(earth, 3);
+            book.AddAstronomicalObject(mars, 4);
+
+            book.RemoveAstronomicalObject(mercury);
+            book.RemoveAstronomicalObject(venus);
+            book.RemoveAstronomicalObject(earth);
+            book.RemoveAstronomicalObject(mars);
+
+            book.ListMaxRangeReached -= OnListMaxRangeReached;
+            book.ListEmptyReached -= OnListEmptyReached;
+
         }
 
     }
