54cb420049893ccd444dde1963c4eeef04ea3565
Sergey Revo <sergey.revo@outlook.com>
2019-12-04 10:19:27 +0200

    MethodExtension

---
+++ Samples/Stage1Chapter17/AstronomicalObjectExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Stage1Chapter14;
+
+
+namespace Stage1Chapter17
+{
+    public static class AstronomicalObjectExtension
+    {
+        public static double GetVolume(this AstronomicalObject astro)
+        {
+            return Math.Round(4/3 * (double)astro.Radius * Math.PI, 0);
+        }
+    }
+}
---
+++ Samples/Stage1Chapter17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Stage1Chapter14;
+
+
+namespace Stage1Chapter17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AstronomicalObject astro = new AstronomicalObject("Earth", 6371, false);
+            Console.WriteLine("Radius of {0} = {1} km", astro.Name, astro.Radius);
+            Console.WriteLine("Volume of {0} = {1} km3", astro.Name, astro.GetVolume());
+            Console.ReadLine();
+        }
+    }
+}
