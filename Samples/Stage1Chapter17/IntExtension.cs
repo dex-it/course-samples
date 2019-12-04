e3c10d902f08a68ab0e1443e740f4392033da63f
Sergey Revo <sergey.revo@outlook.com>
2019-12-04 10:28:21 +0200

    IntToSeconds

---
+++ Samples/Stage1Chapter17/IntExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage1Chapter17
+{
+    public static class IntExtension
+    {
+        public static TimeSpan Seconds(this int intToSeconds)
+        {
+            return new TimeSpan(0, 0, intToSeconds);
+        }
+
+    }
+}
--- Samples/Stage1Chapter17/Program.cs
+++ Samples/Stage1Chapter17/Program.cs
@@ -8,10 +8,19 @@ namespace Stage1Chapter17
     {
         static void Main(string[] args)
         {
+            /*
+            // 1
             AstronomicalObject astro = new AstronomicalObject("Earth", 6371, false);
             Console.WriteLine("Radius of {0} = {1} km", astro.Name, astro.Radius);
             Console.WriteLine("Volume of {0} = {1} km3", astro.Name, astro.GetVolume());
             Console.ReadLine();
+            */
+
+            //2
+            int inToSeconds = 1;
+            Console.WriteLine("{0}.Seconds() = {1} ", inToSeconds, inToSeconds.Seconds());
+            Console.ReadLine();
+
         }
     }
 }
