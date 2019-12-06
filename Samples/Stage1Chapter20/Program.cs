0c1e21901effed8cab86be094e7861b5274396fa
Sergey Revo <sergey.revo@outlook.com>
2019-12-06 15:48:13 +0200

    fix

---
+++ Samples/Stage1Chapter20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage1Chapter20
+{
+    class Program
+    {
+        static void Main()
+        {
+            RegularExpression regExp = new RegularExpression();
+
+            // 1 
+            Console.WriteLine("Введите адрес электронной почты");
+            string email = Console.ReadLine();
+
+            regExp.IsEmailInfo( regExp.IsEmail(email) );
+
+            // 2
+            string numberExp = "(1, 1000, 1 000 000, 100.23)";
+            List<string> listNumExp = regExp.NumberExpression(numberExp);
+            foreach (var s in listNumExp)
+            {
+                Console.WriteLine($"Число: {s}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Samples/Stage1Chapter20/RegularExpression.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Stage1Chapter20
+{
+    public class RegularExpression
+    {
+        public bool IsEmail(string email)
+        {
+            string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+
+        public void IsEmailInfo(bool val)
+        {
+            if (val)
+            {
+                Console.WriteLine("Email подтвержден");
+            }
+            else
+            {
+                Console.WriteLine("Некорректный email");
+            }
+            Console.ReadLine();
+        }
+
+        public List <string> NumberExpression(string strExp)
+        {
+            List<string> listNumExp = new List<string>();
+            
+            string pattern = @"([0-9.](\w*))";
+
+            foreach (Match match in Regex.Matches(strExp, pattern)) 
+            {
+                if (Regex.IsMatch(strExp, pattern))
+                {
+                    listNumExp.Add(match.Groups[0].Value);
+                }
+            }
+            return listNumExp;
+        }
+
+    }
+}
