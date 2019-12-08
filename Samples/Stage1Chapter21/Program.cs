0a0398242ca0e2ba56da1c51f1ce19bc85344941
Revo69 <sergey.revo@outlook.com>
2019-12-08 16:21:31 +0200

    Thread

---
+++ Samples/Stage1Chapter21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stage1Chapter21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Thread(WriteY);
+            t.Start();
+            while (true)
+            {
+                Console.Write("x");
+            }
+        }
+
+        static void WriteY()
+        {
+            while (true)
+            {
+                Console.Write("y");
+            }
+        }
+    }
+}
