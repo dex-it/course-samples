ad064a4a1e9e9fd7792be6e35f9558794ca72c06
Dem-Ivan <demidenko.vanya@list.ru>
2019-12-09 22:45:53 +0300

    add Topic_18(MySerializer)

---
+++ Topic_18(MySerializer)/Topic_18(MySerializer)/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Topic_18_MySerializer_
+{
+	
+	public class Person
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Patronymic { get; set; }
+	} 
+}
---
+++ Topic_18(MySerializer)/Topic_18(MySerializer)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Topic_18_MySerializer_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person person = new Person
+			{
+				Name = "Byll",
+				Surname = "Jonson",
+				Patronymic = "Иванович"
+			};
+
+			MySerializer mySerializer = new MySerializer();
+		
+			foreach (var item in mySerializer.Serialize(person))
+			{
+				Console.WriteLine($"Listt - {item}");
+			}
+
+			// Person p =(Person)mySerializer.Deserializer<Person>(mySerializer.Serialize2(person));
+			mySerializer.Deserializer<Person>(mySerializer.Serialize2(person));
+		}
+	}
+}
