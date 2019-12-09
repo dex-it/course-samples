ad9259f3ddfd8b99b8e514c2d7ba2a05b1b2da43
Sergey Revo <sergey.revo@outlook.com>
2019-12-09 16:57:16 +0200

    Thread

--- Samples/Stage1Chapter21/Program.cs
+++ Samples/Stage1Chapter21/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Stage1Chapter21
 {
@@ -11,20 +9,35 @@ namespace Stage1Chapter21
     {
         static void Main(string[] args)
         {
-            var t = new Thread(WriteY);
-            t.Start();
-            while (true)
-            {
-                Console.Write("x");
-            }
+
+            ThreadingMeasure threadMeasure1 = new ThreadingMeasure();
+            ThreadingMeasure threadMeasure2 = new ThreadingMeasure();
+
+            long startValue = 1;
+            long quantityNumbers = 1000000;
+
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            long avg1 = threadMeasure1.AvgWithotThreads(startValue, quantityNumbers);
+            stopWatch.Stop();
+            Console.WriteLine($"Среднее от {startValue} до {quantityNumbers + startValue - 1} = {avg1}");
+            Console.WriteLine($"Время выполнения: {stopWatch.ElapsedMilliseconds}");
+            Console.WriteLine($"sumFirstHalf: {threadMeasure1.sumFirstHalf}, sumSecondHalf: {threadMeasure1.sumFirstHalf}");
+            Console.WriteLine("");
+
+
+
+            stopWatch.Start();
+            long avg2 = threadMeasure2.AvgWithThreads(startValue, quantityNumbers);
+            stopWatch.Stop();
+            Console.WriteLine($"Среднее от {startValue} до {quantityNumbers + startValue - 1} = {avg2}");
+            Console.WriteLine($"Время выполнения: {stopWatch.ElapsedMilliseconds}");
+            Console.WriteLine($"sumFirstHalf: {threadMeasure2.sumFirstHalf}, sumSecondHalf: {threadMeasure2.sumFirstHalf}");
+            Console.ReadLine();
+
         }
 
-        static void WriteY()
-        {
-            while (true)
-            {
-                Console.Write("y");
-            }
-        }
+
     }
 }
---
+++ Samples/Stage1Chapter21/ThreadingMeasure.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stage1Chapter21
+{
+    public class ThreadingMeasure
+    {
+        public long sumFirstHalf = 0;
+        public long sumSecondHalf = 0;
+
+        public long AvgWithThreads(long startValue, long quantityNumbers)
+        {
+            long middleRange = quantityNumbers / 2;
+
+            Storage storage1 = new Storage
+            {
+                startValue = startValue,
+                quantityNumbers = middleRange,
+                indexSum = 1
+            };
+
+            Thread m = new Thread(new ParameterizedThreadStart(Sum));
+            m.Start(storage1);
+
+            Storage storage2 = new Storage
+            {
+                startValue = startValue + middleRange,
+                quantityNumbers = quantityNumbers - middleRange,
+                indexSum = 2
+            };
+
+            Sum(storage2);
+
+            return (sumFirstHalf + sumSecondHalf) / quantityNumbers;
+        }
+
+        public long AvgWithotThreads(long startValue, long quantityNumbers)
+        {
+
+            long middleRange = quantityNumbers / 2;
+
+            Storage storage1 = new Storage
+            {
+                startValue = startValue,
+                quantityNumbers = middleRange,
+                indexSum = 1
+            };
+
+            Sum(storage1);
+
+            Storage storage2 = new Storage
+            {
+                startValue = startValue + middleRange,
+                quantityNumbers = quantityNumbers - middleRange,
+                indexSum = 2
+            };
+
+            Sum(storage2);
+
+            return (sumFirstHalf + sumSecondHalf) / quantityNumbers;
+
+        }
+
+        public void Sum(object obj)
+        {
+            Storage storage = (Storage)obj;
+
+            long summ = 0;
+            for (long i = storage.startValue; i <= (storage.startValue + storage.quantityNumbers - 1); i++)
+            {
+                summ += i;
+            }
+
+            if (storage.indexSum == 1)
+            {
+                sumFirstHalf = summ;
+            }
+            else
+            {
+                sumSecondHalf = summ;
+            }
+        }
+
+        private class Storage
+        {
+            public long startValue;
+            public long quantityNumbers;
+            public long indexSum;
+        }
+    }
+}
