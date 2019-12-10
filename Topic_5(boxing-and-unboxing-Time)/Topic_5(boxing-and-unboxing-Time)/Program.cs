a3e229d3f94bd69bdd0643480128dd38b7a5c264
Dem-Ivan <demidenko.vanya@list.ru>
2019-12-10 12:44:45 +0300

    changed Topic_5(boxing-and-unboxing-Time)

--- Topic_5(boxing-and-unboxing-Time)/Topic_5(boxing-and-unboxing-Time)/Program.cs
+++ Topic_5(boxing-and-unboxing-Time)/Topic_5(boxing-and-unboxing-Time)/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Topic_5_boxing_and_unboxing_Time_
 {
@@ -10,23 +11,20 @@ namespace Topic_5_boxing_and_unboxing_Time_
 			int a = 123;
 			int b;
 			object Sempl1;
-			long t1;
-			long t2;			
-			TimeSpan procesDuration;
+			var time1 = new Stopwatch();
+			var time2 = new Stopwatch();
 
 			Console.WriteLine("_ _ _ boxing _ _ _");
-			t1 = DateTime.Now.Ticks;
-			Sempl1 = a;
-			t2 = DateTime.Now.Ticks;			
-			procesDuration = new TimeSpan(t2 - t1);
-			Console.WriteLine($"processDuration = {procesDuration.Ticks} ticks    t1 = {t1} t2 = {t2}");
+			time1.Start();			
+			Sempl1 = a;			
+			time1.Stop();		
+			Console.WriteLine($"processDuration = {time1.Elapsed}");
 
-			Console.WriteLine("_ _ _ unboxing _ _ _");			
-			t1 = DateTime.Now.Ticks;
+			Console.WriteLine("_ _ _ unboxing _ _ _");
+			time2.Start();
 			b = (int)Sempl1;
-			t2 = DateTime.Now.Ticks;
-			procesDuration = new TimeSpan(t2 - t1);
-			Console.WriteLine($"processDuration = {procesDuration.Ticks} ticks    t1 = {t1} t2 = {t2}");
+			time2.Stop();
+			Console.WriteLine($"processDuration = {time2.Elapsed}");
 
 		}
 	}
