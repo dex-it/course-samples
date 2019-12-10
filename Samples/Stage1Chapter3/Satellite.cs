84fc02bdb75742993fb9c91ff273d3e081c68f6b
Sergey Revo <sergey.revo@outlook.com>
2019-12-10 15:19:19 +0200

    fix

--- Samples/Stage1Chapter3/Satellite.cs
+++ Samples/Stage1Chapter3/Satellite.cs
@@ -2,8 +2,6 @@
 {
     public class Satellite: AstronomicalObject
     {
-        //protected new bool LightEmission => false;
-
         public Planet ParentPlanet { get; set; }
 
         public Satellite(string satelliteName, decimal radius) :base(satelliteName, radius, false)
