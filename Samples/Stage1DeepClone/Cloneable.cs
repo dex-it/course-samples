79334850f6b70776621160002875e608a3ef7720
Sergey Revo <sergey.revo@outlook.com>
2019-12-10 15:15:01 +0200

    DeepClone

---
+++ Samples/Stage1DeepClone/Cloneable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace Stage1DeepClone
+{
+    public class Cloneable
+    {
+        public object DeepCopyMine(object obj)
+        {
+            if (obj == null) return null;
+
+            object newCopy = Activator.CreateInstance(obj.GetType(), true);
+          
+            foreach (var property in newCopy.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+            {
+                //Console.WriteLine($"{property.Name} - {property.GetValue(obj)} - {property.PropertyType.IsValueType}");
+
+                if (!property.PropertyType.IsValueType && property.PropertyType != typeof(string))
+                {
+                    //Console.WriteLine($"{property.Name} - {property.GetValue(obj)}");
+                    var propertyCopy = DeepCopyMine(property.GetValue(obj));
+                    property.SetValue(newCopy, propertyCopy);
+                }
+                else
+                {
+                    //Console.WriteLine($"{property.Name} - {property.GetValue(obj)}"); 
+                    property.SetValue(newCopy, property.GetValue(obj));
+                }
+            }
+            return newCopy;
+        }
+    }
+}
---
+++ Samples/Stage1DeepClone/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Stage1Chapter3;
+
+namespace Stage1DeepClone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var earth = new Planet("Earth", 6371);
+            
+            var moon = new Satellite("Moon", 1737)
+            {
+                ParentPlanet = earth
+            };
+            
+            Cloneable cloneObject = new Cloneable();
+            var moonNew = (Satellite)cloneObject.DeepCopyMine(moon);
+
+            Console.WriteLine($"ParentPlanet.Name: {moonNew.ParentPlanet.Name}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
