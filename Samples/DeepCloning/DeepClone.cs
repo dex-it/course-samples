8c2c397245ec1f19db2c8225424abf96fa9414b4
VyasheslavKovalch <vyacheslavkovalch@gmail.com>
2019-12-11 22:20:15 +0200

    Add project DeepCloning

---
+++ Samples/DeepCloning/DeepClone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace DeepCloning
+{
+    [Serializable]
+    public class House
+    {
+        public int FloorCount { get; set; } = 2;
+        public List<string> RoomsList = new List<string>();
+        
+        public House DeepCopy()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream memStream = new MemoryStream();
+            formatter.Serialize(memStream, this);
+            memStream.Flush();
+            memStream.Position = 0;
+            return (House)formatter.Deserialize(memStream);
+        }
+    }
+}
---
+++ Samples/DeepCloning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DeepCloning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House sourceHouse = new House();
+            sourceHouse.RoomsList.Add("зал");
+            sourceHouse.RoomsList.Add("спальня");
+            sourceHouse.RoomsList.Add("детская комната");
+            sourceHouse.RoomsList.Add("кухня");
+            sourceHouse.RoomsList.Add("ванная комната");
+
+            House cloneHouse = sourceHouse.DeepCopy();
+
+            Console.ReadLine();
+
+        }
+    }
+}
