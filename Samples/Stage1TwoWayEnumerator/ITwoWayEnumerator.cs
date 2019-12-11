395cbd04ac2e3bd84231bcc8cc0a8e2ea60666da
Sergey Revo <sergey.revo@outlook.com>
2019-12-11 16:02:21 +0200

    TwoWayEnumerator

---
+++ Samples/Stage1TwoWayEnumerator/ITwoWayEnumerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage1TwoWayEnumerator
+{
+    public interface ITwoWayEnumerator<T> : IEnumerator<T>
+    {
+        bool MovePrevious();
+    }
+}
---
+++ Samples/Stage1TwoWayEnumerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stage1TwoWayEnumerator
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            foreach (var a in arr)
+            {
+                Console.WriteLine($"{a}");
+            }
+
+        }
+
+    }
+}
