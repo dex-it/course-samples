cd544648ff34a2872ace0c6b9f7773912fbf6839
VyacheslavKovalch <vyacheslavkovalch@gmail.com>
2019-12-23 02:44:31 +0300

    Add class TaskQueue to project Stage1Paragraph21

---
+++ Samples/Stage1Paragraph21/TaskQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Stage1Paragraph21
+{
+    public class TaskQueue : IJobExecutor
+    {
+        private BlockingCollection<Action> queue;
+        private Task[] parallelTasks;
+        
+        public int Amount => queue.Count;
+
+        public TaskQueue()
+        {
+            queue = new BlockingCollection<Action>();
+        }
+        
+        public void Start(int maxConcurrent)
+        {
+            parallelTasks = new Task[maxConcurrent];
+
+            for (int i = 0; i < maxConcurrent; i++)
+            {
+                parallelTasks[i] = new Task(queue.Take());
+            }
+        }
+
+        public void Stop()
+        {
+            foreach (var task in parallelTasks)
+            {
+                task.Start();
+            }
+        }
+
+        public void Add(Action action)
+        {
+            queue.Add(action);
+        }
+
+        public void Clear()
+        {
+            // первый вариант
+            queue = new BlockingCollection<Action>();
+            
+            // второй вариант
+            //while (queue.Count > 0)
+            //{
+            //    queue.Take();
+            //}
+        }
+    }
+}
