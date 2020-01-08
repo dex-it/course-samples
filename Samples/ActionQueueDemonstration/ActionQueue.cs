cfd47aa7c40c38f7f19d09565271b0ba50a64524
VyacheslavKovalch <vyacheslavkovalch@gmail.com>
2020-01-09 02:22:49 +0300

    Add ActionQueueDemonstration

---
+++ Samples/ActionQueueDemonstration/ActionQueue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ActionQueueDemonstration
+{
+    public class ActionQueue : IJobExecutor
+    {
+        private object _locker = new object();
+        private Thread[] _consumers;
+        private Queue<Action> _queue = new Queue<Action>();
+
+        public int Amount => _queue.Count;
+
+        private void Consume()
+        {
+            while (true) 
+            { 
+                Action item;
+                
+                lock (_locker)
+                {
+                    while (_queue.Count == 0)
+                    {
+                        Monitor.Wait (_locker);
+                    }
+                    
+                    item = _queue.Dequeue();
+                }
+
+                if (item == null)
+                {
+                    return;
+                } 
+                
+                item(); 
+            }
+        }
+        
+        public void Start(int maxConcurrent)
+        {
+            _consumers = new Thread [maxConcurrent];
+
+            for (var i = 0; i < maxConcurrent; i++)
+            {
+                _consumers[i] = new Thread(Consume);
+                _consumers[i].Start();
+            }
+        }
+        
+        public void Stop ()
+        {
+            foreach (Thread consumer in _consumers)
+            {
+                Add (null);
+                //consumer.Join();
+            }
+        }
+
+        public void Add (Action item)
+        {
+            lock (_locker)
+            {
+                _queue.Enqueue (item);
+                Console.WriteLine($"{Thread.CurrentThread.Name}: добавлена задача, (всего: {Amount})");
+                Monitor.Pulse (_locker); 
+            }
+        }
+
+        public void Clear()
+        {
+            lock (_locker)
+            {
+                _queue.Clear();
+                Console.WriteLine("Очередь очищена");
+                Monitor.Pulse (_locker);
+            }
+        }
+    }
+}
---
+++ Samples/ActionQueueDemonstration/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace ActionQueueDemonstration
+{
+    internal class Program
+    {
+        private static void FillQueue(ActionQueue queue)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                queue.Add(() =>
+                {
+                    Console.WriteLine("Выполнение задачи");
+                });
+                Thread.Sleep(1000);
+            }
+        }
+        
+        public static void Main()
+        {
+            // Создаем очередь
+            ActionQueue queue = new ActionQueue();
+            
+            // Добавляем в очередь задачи из разных потоков
+            Thread t1 = new Thread(() => FillQueue(queue));
+            t1.Name = "Поставщик 1";
+            t1.Start();
+
+            Thread t2 = new Thread(() => FillQueue(queue));
+            t2.Name = "Поставщик 2";
+            t2.Start();
+
+            Thread t3 = new Thread(() => FillQueue(queue));
+            t3.Name = "Поставщик 3";
+            t3.Start();
+            
+            // Ожидаем некоторое время
+            Thread.Sleep(3000);
+            
+            // Запускаем обработку очереди
+            queue.Start(2);
+            
+            // Ожидаем некоторое время
+            Thread.Sleep(3000);
+            
+            // Останавливаем обработку очереди
+            queue.Stop();
+            
+            // Ожидаем некоторое время
+            Thread.Sleep(5000);
+            
+            // Чистим очередь
+            queue.Clear();
+
+            Console.ReadLine();
+        }
+    }
+}
