f5b546cc0cfcef5d415bcc25de981fd8437600a1
Влад Зброжек <pointbreak22@outlook.com>
2020-02-05 02:01:06 +0200

    событие, реализация интерфейса

---
+++ События/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace События
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.PropertyChanged += DisplayMessage;
+            t.NotifyPropertyChanged("Hello Word!!");
+            t.PropertyChanged -= DisplayMessage;
+            Console.ReadLine();
+        }
+        private static void DisplayMessage(object sender, PropertyChangedEventArgs e)
+        {
+            Console.WriteLine(e.PropertyName);
+        }
+        class Test : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+           public void NotifyPropertyChanged(string msg)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(msg));
+            }
+           
+        }
+    }
+}
