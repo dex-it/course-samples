1c615ce44b8d32b5df7f8f40d126804e70a3888e
Влад Зброжек <pointbreak22@outlook.com>
2020-02-26 14:44:55 +0200

    ...

--- Типы значения,Клонирование/Program.cs
+++ Типы значения,Клонирование/Program.cs
@@ -14,12 +14,12 @@ namespace Типы_значения_Клонирование
             Console.WriteLine(p1.Work.Name); // Microsoft
             Console.Read();
         }
-        class Person : ICloneable
+        struct Person : ICloneable
         {
             public string Name { get; set; }
             public int Age { get; set; }
             public Company Work { get; set; }
-            public object Clone()  //метод для создания нового обьекта сисходным значением так как при простого присвоения копируется ссылка
+            public object Clone()  //метод для создания нового обьекта с исходным значением так как при простого присвоения копируется ссылка
             {Company company = new Company { Name = this.Work.Name }; // создание ссылки на обьект для глубокого копирования
                 return new Person { Name = this.Name, Age = this.Age, Work = company };  //поверхосное копирование, кроме Work
              //   return this.MemberwiseClone();   //для упрощения
@@ -29,5 +29,6 @@ namespace Типы_значения_Клонирование
         {
             public string Name { get; set; }
         }
+
     }
 }
