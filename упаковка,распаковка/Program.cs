b9426a99ab1d317b7846a393f9325dfeb511f2ef
Влад Зброжек <pointbreak22@outlook.com>
2020-02-28 16:20:46 +0200

    упаковка

--- упаковка,распаковка/Program.cs
+++ упаковка,распаковка/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace упаковка_распаковка
@@ -7,16 +8,24 @@ namespace упаковка_распаковка
     {
         static void Main(string[] args)
         {
-            int i = 1000;
-           Stopwatch st = new Stopwatch();
+            int n = 0;
+            object o=new object();
+
+            Stopwatch st = new Stopwatch();
             st.Start();
-            object o = i;
+
+            for (int i = 0; i < 100; i++)
+            { 
+                o= n; //упаковка
+            }
 
             st.Stop();
             string time1 = st.ElapsedTicks.ToString();
             st.Restart();
-          
-            i = (int)o;
+            for (int i = 0; i < 100; i++)
+            {
+                n= (int)o;//расспаковка
+            }
             st.Stop();
             string time2 = st.ElapsedTicks.ToString();
             Console.WriteLine("Время упаковки в тиках:" + time1 + " Время расспаковки:" + time2);
