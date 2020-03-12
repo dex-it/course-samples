1ef201b99ce24028a8ec65af0a0cf94bd7314093
tasha_dexter <tasha_dexter@mail.ru>
2020-03-12 21:08:29 +0200

    Fixed some code (Dictionary)

--- Samples/OlgaVarzyaeva/11 - Dictionary/11 - Dictionary/Program.cs
+++ Samples/OlgaVarzyaeva/11 - Dictionary/11 - Dictionary/Program.cs
@@ -10,7 +10,7 @@ namespace _11___Dictionary
     {
         class Person
         {
-            public Person(){}
+            public Person() { }
             public Person(int passportID, string firstName, string lastName, DateTime birthDate, string birthPlace)
             {
                 PassportID = passportID;
@@ -18,7 +18,6 @@ namespace _11___Dictionary
                 LastName = lastName;
                 BirthDate = birthDate;
                 BirthPlace = birthPlace;
-
             }
             public int PassportID { get; set; }
             public string FirstName { get; set; }
@@ -27,20 +26,18 @@ namespace _11___Dictionary
             public string BirthPlace { get; set; }
             public void ShowInfo()
             {
-                Console.WriteLine("PassportId:"+PassportID.ToString()+"\nFirstName:"+FirstName+
-                    "\nLastName:"+LastName+"\nBirthDate:"+BirthDate.ToString()+"\nBirthPlace:"+BirthPlace);
+                Console.WriteLine("PassportId:" + PassportID.ToString() + "\nFirstName:" + FirstName +
+                    "\nLastName:" + LastName + "\nBirthDate:" + BirthDate.ToString() + "\nBirthPlace:" + BirthPlace);
             }
         }
         static void Main(string[] args)
         {
-            
             Person p1 = new Person(1111, "Иван", "Васильев", Convert.ToDateTime("18.09.1987"), "Тирасполь");
             Person p2 = new Person(2222, "Ксения", "Олейнева", Convert.ToDateTime("11.02.1992"), "Бендеры");
             Person p3 = new Person(3333, "Петр", "Шолохов", Convert.ToDateTime("02.01.1986"), "Тирасполь");
             Person p4 = new Person(4444, "Геннадий", "Серов", Convert.ToDateTime("25.07.1990"), "Слободзея");
             Person p5 = new Person(5555, "Лидия", "Цуканова", Convert.ToDateTime("11.12.1999"), "Тирасполь");
-            Dictionary<Person, string> WorkDictionary = new Dictionary<Person, string> { { p1, "Больница" }, { p2, "Школа №2"}, {p3, "Швейная фабрика" } };
-
+            Dictionary<Person, string> WorkDictionary = new Dictionary<Person, string> { { p1, "Больница" }, { p2, "Школа №2" }, { p3, "Швейная фабрика" } };
             Console.Write("Введите данные для поиска по словарю.\nPassportID:");
             int PassportID = Int32.Parse(Console.ReadLine());
             Console.Write("FirstName:");
@@ -58,16 +55,13 @@ namespace _11___Dictionary
                     && p.Key.BirthDate == BirthDate && p.Key.BirthPlace == BirthPlace)
 
                 {
-                    Console.WriteLine("Work:"+p.Value);
+                    Console.WriteLine("Work:" + p.Value);
                     k++;
                 }
             }
-            if(k==0)
+            if (k == 0)
                 Console.WriteLine("Человек отсутствует в базе данных!");
-
-
             Console.ReadKey();
-
         }
     }
 }
