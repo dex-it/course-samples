00c65ccbd9db51fb57156cf8bc5565afbe20e83d
Влад Зброжек <pointbreak22@outlook.com>
2020-03-15 13:41:31 +0200

    потоки

---
+++ Потоки/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+
+namespace Потоки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double[] mas1 = new double[10];
+            double[] mas2 = new double[100000000];
+            double Sum1 = 0;
+            var locker = new object();
+            Mutex mutexObj = new Mutex();
+
+            for (int i = 0; i < mas1.Length; i++)
+            {
+                mas1[i] = random.Next(100);
+                Console.WriteLine(mas1[i]);
+            }
+            bool acquiredLock = false;
+            int n = mas1.Length;
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+
+                while (n-- > 0)
+                {
+                    lock (locker)
+                    {
+                        Sum1 += mas1[n];
+                    }
+                }
+
+
+                if (n==0) mutexObj.ReleaseMutex();
+            });
+
+
+            mutexObj.WaitOne();
+            Console.WriteLine("Cумма =" + Sum1);
+            Console.ReadLine();
+        }
+    }
+}
