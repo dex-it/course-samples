19494c343a79d6af071bfeaecec1d82fa39481b3
Влад Зброжек <pointbreak22@outlook.com>
2020-03-19 02:14:04 +0200

    готовое потоки задание 1

--- Потоки/Program.cs
+++ Потоки/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using System.Linq;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Потоки
 {
@@ -9,37 +11,90 @@ namespace Потоки
         static void Main(string[] args)
         {
             Random random = new Random();
-            double[] mas1 = new double[10];
+            double[] mas1 = new double[10000000];
             double[] mas2 = new double[100000000];
             double Sum1 = 0;
-            var locker = new object();
-            Mutex mutexObj = new Mutex();
+           // var locker = new object();
+            EventWaitHandle eventWait = new EventWaitHandle(false, EventResetMode.AutoReset); //обьявление синхронизации
+
+            Stopwatch stopwatch = new Stopwatch();  //создание тикомера
 
             for (int i = 0; i < mas1.Length; i++)
             {
                 mas1[i] = random.Next(100);
-                Console.WriteLine(mas1[i]);
+                //Console.Write(mas1[i]+" ");
             }
             bool acquiredLock = false;
             int n = mas1.Length;
-            ThreadPool.QueueUserWorkItem(_ =>
-            {
 
+            Console.WriteLine("10М переменных");
+            ThreadPool.QueueUserWorkItem(_ =>    //обработка пулл потоков
+            {
+                if (!stopwatch.IsRunning) stopwatch.Start();//запуск тикомера
                 while (n-- > 0)
                 {
-                    lock (locker)
-                    {
+                    //lock (locker)
+                   // {
                         Sum1 += mas1[n];
-                    }
+                    //}
                 }
-
-
-                if (n==0) mutexObj.ReleaseMutex();
+                if (n <= 0)
+                {
+                    stopwatch.Stop(); //остановка тикомера
+                    Console.WriteLine("Cреднее параллельной обработки=" + (Sum1 / mas1.Length).ToString());
+                    Console.WriteLine("Время 1: " + stopwatch.ElapsedMilliseconds.ToString() + " млс");
+                    eventWait.Set();//активация
+                }
             });
+            eventWait.WaitOne();//прерывание
+            Sum1 = 0;
+            if (!stopwatch.IsRunning) stopwatch.Restart();
+            for (int i = 0; i < mas1.Length; i++)
+                Sum1+= mas1[i];
+            stopwatch.Stop();
+       
+            Console.WriteLine("Cреднее последовательной обработки=" + (Sum1 / mas1.Length).ToString());
+            Console.WriteLine("Время 2: " + stopwatch.ElapsedMilliseconds.ToString() + " млс");
+          //  stopwatch.Reset();
 
+            //------------------------------------------------------------------
+           // eventWait.Reset();   //если мануал
+            for (int i = 0; i < mas2.Length; i++)
+            {
+                mas2[i] = random.Next(100);
+                //Console.Write(mas2[i]+" ");
+            }
+   
+            n = mas2.Length;
+            Sum1 = 0;
+            Console.WriteLine("100М переменных");
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                if (!stopwatch.IsRunning) stopwatch.Restart();
+                while (n-- > 0)
+                {
+                    //lock (locker)
+                    // {
+                    Sum1 += mas2[n];
+                    //}
+                }
+                stopwatch.Stop();
+              
+                Console.WriteLine("Cреднее параллельной обработки=" + (Sum1 / mas2.Length).ToString());
+                Console.WriteLine("Время 1: " + stopwatch.ElapsedMilliseconds.ToString() + " млс");
+              //  stopwatch.Reset();
+                eventWait.Set();
+            });
+            eventWait.WaitOne();
+            Sum1 = 0;
+            if (!stopwatch.IsRunning) stopwatch.Restart();
+            for (int i = 0; i < mas2.Length; i++)
+                Sum1 += mas2[i];
+            stopwatch.Stop();
+          
+            Console.WriteLine("Cреднее последовательной обработки=" + (Sum1 / mas2.Length).ToString());
+            Console.WriteLine("Время 2: " + stopwatch.ElapsedMilliseconds.ToString() + " млс");
 
-            mutexObj.WaitOne();
-            Console.WriteLine("Cумма =" + Sum1);
             Console.ReadLine();
         }
     }
