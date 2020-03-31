9711b1137113c0ee668737d60887eb54f16d2fed
tasha_dexter <tasha_dexter@mail.ru>
2020-03-31 20:20:07 +0300

    Added 14 - Events

---
+++ Samples/OlgaVarzyaeva/14 - Events/14 - Events/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _14___Events
+{
+    class Program
+    {
+        public class Gamer : INotifyPropertyChanged
+        {
+            private string LoginValue = String.Empty;
+            private int IDValue = 0;
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                Console.WriteLine("Внимание! Было изменено: "+ propertyName);
+            }
+
+            private Gamer()
+            {
+                LoginValue = "Gamer";
+                IDValue = 123;
+            }
+            public static Gamer CreateNewGamer()
+            {
+                return new Gamer();
+            }
+
+            public string Login
+            {
+                get
+                {
+                    return this.LoginValue;
+                }
+
+                set
+                {
+                    if (value != this.LoginValue)
+                    {
+                        this.LoginValue = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+
+            public int ID
+            {
+                get
+                {
+                    return this.IDValue;
+                }
+                set
+                {
+                    if (value != this.IDValue)
+                    {
+                        this.IDValue = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            BindingList<Gamer> gamerList = new BindingList<Gamer>();
+            gamerList.Add(Gamer.CreateNewGamer());
+            gamerList.Add(Gamer.CreateNewGamer());
+            gamerList.Add(Gamer.CreateNewGamer());            
+            gamerList[0].Login = "Destructor!!!";
+            gamerList[0].ID = 25;
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Samples/OlgaVarzyaeva/14 - Events/14 - Events/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("14 - Events")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("14 - Events")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0af4b61c-c52a-4efc-809a-51f6c210bdfe")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
