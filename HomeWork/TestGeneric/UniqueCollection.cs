600aed5e8e36c6266c122453d841602434b85afe
stron333 <stron333@list.ru>
2020-05-29 12:42:42 +0300

    Modify UniqCollection

--- HomeWork/TestGeneric/UniqueCollection.cs
+++ HomeWork/TestGeneric/UniqueCollection.cs
@@ -9,22 +9,12 @@ namespace TestUniqueCollections
     {
         public new void Add(T item)
         {
-            bool isExist = false;
-            for (int i = 0; i < this.Count; i++)
-            {
-                if (this[i].GetHashCode() == item.GetHashCode())
-                {
-                    isExist = true;
-                }
-            }
-
-            if (!isExist)
+            if (!this.Contains(item))
                 base.Add(item);
             else
             {
                 throw new Exception("Такой элемент уже существует");
             }
-
         }
     }
 }
