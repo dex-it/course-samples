e4f86bb191374523bc01b39b0cc8cdd3c683c8fd
stron333 <stron333@list.ru>
2020-06-16 22:41:59 +0300

    НАчал расширения

---
+++ HomeWork/17 Extentions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _17_Extentions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите символ:");
+            char ch = Console.ReadKey().KeyChar;
+
+            Console.WriteLine();
+            Console.WriteLine(name.MakeBorder(ch));
+            Console.ReadKey();
+        }
+    }
+    public static class StringExtensions
+    {
+        private static char _ch { get; set; }
+        public static string MakeBorder(this string str, char ch)
+        {
+            if (str == null || str == "") throw new ArgumentNullException(nameof(str));
+            _ch = ch;
+            return PrintChar(str.Length+2)+Environment.NewLine+
+                   PrintChar(1)+ str+ PrintChar(1) + Environment.NewLine +
+                   PrintChar(str.Length + 2);
+        }
+        private static string PrintChar (int count)
+        {
+            return new string (_ch, count);
+        }
+    }
+}
