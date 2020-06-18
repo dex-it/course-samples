223381526ce97e71d9d0103d994c367fa9126f27
Александр Рыбарук <stron333@ro.ru>
2020-06-18 13:46:34 +0300

    Extentions done

---
+++ HomeWork/17 Extentions/ExtentionsInt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17_Extentions
+{
+    public static class ExtentionsInt
+    {
+        public static TimeSpan Hours(this int hours)
+        {
+            return new TimeSpan(hours, 0, 0);
+        }
+        public static TimeSpan Minutes(this int minutes)
+        {
+            int hours = minutes / 60;
+            minutes = minutes - (hours * 60);
+            return new TimeSpan(hours, minutes, 0);
+        }
+        public static TimeSpan Seconds(this int seconds)
+        {
+            int minutes = seconds / 60;
+            seconds = seconds - (minutes * 60);
+            return minutes.Minutes()+ new TimeSpan(0,0, seconds);
+        }
+    }
+}
---
+++ HomeWork/17 Extentions/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _17_Extentions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                string numbers = "";
+                ConsoleKeyInfo btn;
+                Console.WriteLine("Введите число и нажмите Enter. Для выхода нажмите Esc");
+                do
+                {
+                    int parse;
+                    btn = Console.ReadKey(true);
+                    if (btn.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }else if (btn.Key == ConsoleKey.Backspace)
+                    {
+                        if (!String.IsNullOrEmpty(numbers))
+                            numbers = numbers.Remove(numbers.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                    else
+                    {
+                        if (Char.IsDigit(btn.KeyChar))
+                        {
+                            numbers += btn.KeyChar.ToString();
+                            Console.Write(btn.KeyChar);
+                        }
+                    }
+                } while (btn.Key != ConsoleKey.Escape);
+
+                if (btn.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                else
+                {
+                    int seconds;
+                    if (int.TryParse(numbers, out seconds))
+                    {
+                        Console.WriteLine(Environment.NewLine + seconds.Seconds());
+                    }
+                }
+            } 
+        }
+
+    }
+}
