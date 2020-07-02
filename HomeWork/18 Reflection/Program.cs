faa232ee78cd4fb9754f21089956c146ab40900e
Александр Рыбарук <stron333@ro.ru>
2020-07-02 15:01:32 +0300

    Рефлексируем

---
+++ HomeWork/18 Reflection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace _18_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myType = Type.GetType("TestIComparable.SomeFigure, 9_TestIComparable", true, true);
+
+            Console.ReadKey();
+        }
+    }
+}
