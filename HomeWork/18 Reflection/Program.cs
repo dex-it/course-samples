0c89cc2b3641ca0bea5d6817221b26fd68e96fc3
Александр Рыбарук <stron333@ro.ru>
2020-07-03 11:37:46 +0300

    Конец рефлексии

--- HomeWork/18 Reflection/Program.cs
+++ HomeWork/18 Reflection/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ConsoleApp1;
 
 namespace _18_Reflection
 {
@@ -7,7 +10,29 @@ namespace _18_Reflection
     {
         static void Main(string[] args)
         {
-            Type myType = Type.GetType("TestIComparable.SomeFigure, 9_TestIComparable", true, true);
+            //получаю тип внешнего клаксса
+            Type type = Type.GetType("ConsoleApp1.SmartPhone, ConsoleApp1", true, true);
+
+            //делаю инстанс этого класса
+            object objPhoneWithCamera = Activator.CreateInstance(type);
+            //получаю методы Сделать фото и переименовать фото
+            MethodInfo methodMakePhoto = type.GetMethod("MakePhoto");
+            MethodInfo methodRenamePhoto = type.GetMethod("RenamePhoto");
+
+            //выполняю эти методы
+            methodMakePhoto.Invoke(objPhoneWithCamera, new object[] {});
+            methodRenamePhoto.Invoke(objPhoneWithCamera, new[] {"NewNameForPhoto"});
+
+            //Получаю поле Photo внешнего класса
+            var photo = type.GetProperty("Photo").GetValue(objPhoneWithCamera);
+            //Получаю поле Name класса Photo
+            var namePhoto = photo.GetType().GetProperty("Name").GetValue(photo);
+
+
+            Console.WriteLine(namePhoto);
+
+
+
 
             Console.ReadKey();
         }
