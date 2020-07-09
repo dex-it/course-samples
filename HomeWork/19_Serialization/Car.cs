4eac4ba8b74228bbdb4aeb26411eaabe0ae63f97
stron333 <stron333@list.ru>
2020-07-09 20:44:50 +0300

    сериализация

---
+++ HomeWork/19_Serialization/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Serialization
+{
+    [Serializable]
+    public class Car
+    {
+        public Colors Color { get; set; }
+        public int YearOfIssue { get; set; }
+        public Engine engine { get; set; }
+
+        public class Engine
+        {
+            public double Volume { get; set; }
+            public TypeOfFuel TypeOfFuel { get; set; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Год выпуска {0}; " +
+                                 "Цвет: {1}; " +
+                                 "Объем двигателя {2}; " +
+                                 "Тип топлива: {3}", 
+                YearOfIssue, Color, engine.Volume, engine.TypeOfFuel);
+        }
+    }
+
+    public enum TypeOfFuel
+    {
+        Petrol95,
+        Petrol92,
+        Diesel
+    }
+
+    public enum Colors
+    {
+        Red,
+        White,
+        Black,
+        Silver
+    }
+}
--- HomeWork/19_Serialization/Program.cs
+++ HomeWork/19_Serialization/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace _19_Serialization
 {
@@ -10,6 +12,61 @@ namespace _19_Serialization
     {
         static void Main(string[] args)
         {
+            Car[] CarArray = FillTheArray();
+            Serialize(CarArray);
+
+            Car[] DeserializeCarArray = Deserialize();
+
+            foreach (var car in DeserializeCarArray)
+            {
+                Console.WriteLine(car.ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+        public static Car[] FillTheArray ()
+        {
+            return new Car[] {
+                new Car
+                {
+                    YearOfIssue = 2010,
+                    Color = Colors.Black,
+                    engine = new Car.Engine{TypeOfFuel = TypeOfFuel.Petrol95, Volume = 2.0}
+                },
+                new Car 
+                {
+                    YearOfIssue = 2015,
+                    Color = Colors.Silver,
+                    engine = new Car.Engine { TypeOfFuel = TypeOfFuel.Diesel, Volume = 2.2 }
+                }, new Car
+                {
+                    YearOfIssue = 1993,
+                    Color = Colors.Red,
+                    engine = new Car.Engine { TypeOfFuel = TypeOfFuel.Petrol92, Volume = 1.6 }
+                } };
+        }
+
+        public static void Serialize(Car[] cars)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Car[]));
+            using (FileStream fs = new FileStream("cars.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fs, cars);
+            }
+        }
+
+        public static Car[] Deserialize()
+        {
+            Car[] cars = new Car[] { };
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Car[]));
+
+            using (FileStream fs = new FileStream("cars.xml", FileMode.OpenOrCreate))
+            {
+                cars = (Car[])xmlSerializer.Deserialize(fs);
+            }
+
+            return cars;
         }
     }
 }
