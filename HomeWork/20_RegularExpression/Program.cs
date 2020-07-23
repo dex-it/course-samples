fb372bd12cc0d3081582eb74f16c5c6bd4765b6b
Александр Рыбарук <stron333@ro.ru>
2020-07-23 10:46:34 +0300

    Test Regular

---
+++ HomeWork/20_RegularExpression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _20_RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "Какой-то текст, который от другого текста не тексту отличается";
+            using (WebClient webClient = new WebClient())
+                line = webClient.DownloadString("http://www.cbr.ru/scripts/XML_daily.asp");
+            Match match = Regex.Match(line, "<Name>Доллар США</Name><Value>(.*?)</Value>");
+
+            Console.WriteLine(match.Groups[1].Value);
+           
+
+            Console.ReadLine();
+        }
+    }
+}
